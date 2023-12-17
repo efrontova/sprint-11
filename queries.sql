2cd346a4912f23c5ee21f12e1296b87fb6148ab0
e_frontova <e_frontova@mail.ru>
2023-12-17 11:43:36 +0300

    Fix SQL query

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 -- Задание 1 --  вывести список логинов курьеров с количеством их заказов в статусе «В доставке».
-SELECT c.login, COUNT(o.id) FROM "Couriers" AS c LEFT JOIN "Orders" AS o ON c.id = o."courierId" AND o."inDelivery" = true GROUP BY c.login;
+SELECT c.login, COUNT(*) FROM "Couriers" AS c INNER JOIN "Orders" AS o ON c.id = o."courierId" WHERE o."inDelivery" = true GROUP BY c.login;
 
 -- Задание 2 -- вывести все трекеры заказов и их статусы.
 SELECT track, CASE WHEN finished = true then '2' WHEN cancelled = true then '-1' WHEN "inDelivery"=true then '1' ELSE '0' END AS status FROM "Orders" LIMIT 10;
